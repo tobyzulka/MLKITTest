8510c388f1c02f6b8b2ad1c93978a572ba035d70
Toby Zulkarnain <toby.zulka@gmail.com>
2019-08-05 22:57:41 +0700

    90 persen jadi

--- app/src/main/java/com/tobyz/mlkittest/MainActivity.kt
+++ app/src/main/java/com/tobyz/mlkittest/MainActivity.kt
@@ -3,48 +3,35 @@ package com.tobyz.mlkittest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
-import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_proses.*
-import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
+
     companion object {
         val REQUEST_IMAGE_CAPTURE = 1
+
         val IMAGE_PICK_CODE = 2
-        val PERMISSION_CODE= 3
-        var currentPhotoPath: String = ""
+        private val PERMISSION_CODE = 10
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //Tombol Camera
         ibtn_camera.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if(checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
-                    PackageManager.PERMISSION_DENIED) {
-                    val permission = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    requestPermissions(permission, PERMISSION_CODE)
-                } else {
-                    dispatchTakePictureIntent()
-                }
-            } else {
-                dispatchTakePictureIntent()
-            }
+            dispatchTakePictureIntent()
         }
+
         // Tombol Ambil citra dari Gallery
         ibtn_image.setOnClickListener {
-//            selectImageInAlbum()
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
                         PackageManager.PERMISSION_DENIED) {
@@ -59,68 +46,43 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Ambil citra dari Camera
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(packageManager)?.also {
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    null
-                }
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                        this,
-                        "com.tobyz.mlkittest.fileprovider",
-                        it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-
-                    galleryAddPic(takePictureIntent)
-                }
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
             }
         }
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE ){
-            imgCapture.setImageURI(data?.data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            sendBitmap(imageBitmap)
         }
-    }
-
-    // Tambah citra dari camera ke storage
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Buat penamaan citra berdasarkan tanggal
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "OBYLENS_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // simpan citra: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
+        else if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE ){
+            val dataIamge = data?.data
+            sendUri(dataIamge)
         }
-    }
-
-    // Tampilkan citra dalam Gallery
-    private fun galleryAddPic(file: Intent) {
-        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
-            val f = File(currentPhotoPath)
-            mediaScanIntent.data = Uri.fromFile(f)
-            sendBroadcast(mediaScanIntent)
-        }
-//        val f = File(file)
-//        val contentUri = Uri.fromFile(f)
-//        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, contentUri)
-//        sendBroadcast(mediaScanIntent)
     }
 
     private fun pickImageFromGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    fun sendUri(uri: Uri?){
+        val i = Intent(this, ProsesActivity::class.java)
+        i.setData(uri)
+        startActivity(i)
+    }
+
+    fun sendBitmap(bitmap: Bitmap){
+        val i = Intent(this, ProsesActivity::class.java)
+        val bs = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 50, bs)
+        i.putExtra("image", bs.toByteArray())
+        startActivity(i)
     }
 }
--- app/src/main/java/com/tobyz/mlkittest/ProsesActivity.kt
+++ app/src/main/java/com/tobyz/mlkittest/ProsesActivity.kt
@@ -1,12 +1,81 @@
 package com.tobyz.mlkittest
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_proses.*
+import android.graphics.BitmapFactory
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import android.widget.Toast
+import com.google.firebase.ml.vision.FirebaseVision
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
 
 class ProsesActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_proses)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        ibtn_image.setOnClickListener {
+            copy(txtResult.text.toString())
+        }
+        detect()
+
+
+    }
+
+    fun detect() {
+        val bundle = getIntent().extras
+        val image_path = intent.data
+        val image: FirebaseVisionImage
+        if (bundle != null) {
+            val b = BitmapFactory.decodeByteArray(intent.getByteArrayExtra("image"), 0, intent.getByteArrayExtra("image").size)
+            imgCapture.setImageBitmap(b)
+            image = FirebaseVisionImage.fromBitmap(b)
+            recognizeText(image)
+        }
+        else if(image_path!=null){
+            try {
+                imgCapture.setImageURI(image_path)
+                image = FirebaseVisionImage.fromFilePath(this, image_path)
+                recognizeText(image)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        else {
+            Toast.makeText(applicationContext, "Gambar Kosong!!!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun recognizeText(image: FirebaseVisionImage) {
+        val detector = FirebaseVision.getInstance()
+            .onDeviceTextRecognizer
+            detector.processImage(image)
+            .addOnSuccessListener { firebaseVisionText ->
+                txtResult.setText(firebaseVisionText.text)
+            }
+            .addOnFailureListener {e ->
+                e.printStackTrace()
+            }
+
+    }
+
+    fun copy(text: CharSequence){
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("copy text", text)
+        clipboard.primaryClip = clip
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
